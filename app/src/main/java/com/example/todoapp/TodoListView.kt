fa45a0da8c6b4bf0d8c530630d834cbbfed59140
RPIUdishan <it18082234@my.sliit.lk>
2021-03-18 18:17:17 +0530

    remove unwanted commits

--- app/src/main/java/com/example/todoapp/TodoListView.kt
+++ app/src/main/java/com/example/todoapp/TodoListView.kt
@@ -21,39 +21,7 @@ class TodoListView : AppCompatActivity() {
         setContentView(R.layout.activity_todo_list_view)
 
         listOfTodo = findViewById(R.id.todo_listView)
-
-
-//        var db = DBHandeller(this)
-
-        val TAG = "Test"
-//        var emList = listOf<String>()
-//        val data = db.readActivities()
-
-        val tstList = listOf<String>("Mango", "Apple", "Banana")
-//        var x = data.get(2).
-//        Log.d(TAG, data.get(1).todo.toString())
-//        Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::After data")
-//        var itr = data.listIterator()
-//        Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::After Iteration")
-
-//        for (element in data){
-//            Log.d(TAG, element.todo)
-//            emList.toMutableList().add(element.todo)
-//        }
-
-//        while(itr.hasNext()){
-//            Log.d(TAG, itr.next().todo.toString())
-//            Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::Before Looping")
-//            emList.toMutableList().add(itr.next().todo.toString())
-//            Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::After Looping")
-//        }
-//        Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::After Array While Execution")
-
-//        Log.d(TAG, "QWERTY "+emList.toString())
-
-//        val arrayAdapter: ArrayAdapter<*>
-//        Log.d(TAG, ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::After Array Adaptor Creation")
-//        arrayAdapteryAdapter = MyAdapterpter
+        
         listOfTodo.adapter = MyAdapter(this)
 
 
